50ebef9eba2354f21dd70bc581d07f9495a55e6b
chanchal kumar <chanchalshahkumar@gmail.com>
2020-04-12 13:45:54 +0530

    Config server added

---
+++ PhotoAppApiConfigServer/src/test/java/com/csk/photoapp/api/configserver/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csk.photoapp.api.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
